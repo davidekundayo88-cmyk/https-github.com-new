fb44d4fafdd7ae8cdaec21e5c11b8e28e52eca21
davidekundayo88-cmyk <davidekundayo88@gmail.com>
2025-12-13 12:03:24 -0800

    Create layout.jsx

---
+++ app/layout.jsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: "Dsport Goalkeeper Kit Store",
+  description: "Goalkeeper kits store",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
